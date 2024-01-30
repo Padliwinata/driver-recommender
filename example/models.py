32901c6e57334b0a60611fe6647e0a3141cf0d05
Padliwinata <rpadliwinata@gmail.com>
2024-01-30 09:05:45 +0700

    update

--- example/models.py
+++ example/models.py
@@ -1,3 +1,41 @@
+from enum import Enum
+
 from django.db import models
 
+
+class Faktor(Enum):
+    CORE = 'Core'
+    SECONDARY = 'Secondary'
+
 # Create your models here.
+class Variabel(models.Model):
+    nama = models.CharField(max_length=50)
+    faktor = models.CharField(
+        max_length=10,
+        choices=[(choice.name, choice.value) for choice in Faktor],
+        default=Faktor.CORE.name
+    )
+    persentase = models.IntegerField()
+
+
+class SubVariabel(models.Model):
+    kode = models.CharField(max_length=10)
+    nama = models.CharField(max_length=10)
+    faktor = models.CharField(
+        max_length=10,
+        choices=[(choice.name, choice.value) for choice in Faktor],
+        default=Faktor.CORE.name
+    )
+    standar = models.IntegerField()
+
+class Pegawai(models.Model):
+    id_pegawai = models.AutoField(primary_key=True)
+    nama = models.CharField(max_length=100)
+
+class Skor(models.Model):
+    pegawai = models.ForeignKey(Pegawai, on_delete=models.CASCADE)
+    sub_variabel = models.ForeignKey(SubVariabel, on_delete=models.CASCADE)
+    skor = models.IntegerField()
+
+    
+
