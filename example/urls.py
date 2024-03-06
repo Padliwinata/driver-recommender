c3fcc304a33e25bc06278f973e34367ffc89077a
Padliwinata <rpadliwinata@gmail.com>
2024-03-06 16:43:09 +0700

    update

--- example/urls.py
+++ example/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
-from .views import index, main, login, skor, variabel
+from django.contrib.auth import views as auth_views
+from .views import index, main, skor, variabel, create_variabel, delete_variabel, subvariabel, \
+    delete_subvariabel, create_subvariabel, employee, create_employee, delete_employee
 
 urlpatterns = [
     path('', index, name='index'),
-    path('login', login, name='login'),
+    # path('login', login, name='login'),
     path('main', main, name='main'),
     path('skor', skor, name='skor'),
-    path('variabel', variabel, name='variabel')
+    path('variabel', variabel, name='variabel'),
+    path('create-variabel', create_variabel, name='create-variabel'),
+    path('delete-variabel/<str:var_name>', delete_variabel, name='delete-variabel'),
+    path('delete-subvariabel/<str:kode>', delete_subvariabel, name='delete-subvariabel'),
+    path('subvariabel', subvariabel, name='subvariabel'),
+    path('create-subvariabel', create_subvariabel, name='create-subvariabel'),
+    path('employee', employee, name='employee'),
+    path('create-employee', create_employee, name='create-employee'),
+    path('delete-employee/<str:id_pegawai>', delete_employee, name='delete-employee'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('login', auth_views.LoginView.as_view(template_name='example/login.html'), name='login')
 ]
--- example/views.py
+++ example/views.py
@@ -2,8 +2,10 @@ from collections import defaultdict
 import json
 
 from django.http import HttpResponseForbidden
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from urllib.parse import urlparse
+
 from .models import Pegawai, SubVariabel, Skor, Variabel
 from .utils import get_skor
 
@@ -13,24 +15,25 @@ def index(request):
     return render(request, "example/index.html")
 
 
-def login(request):
-    if request.method == "GET":
-        return render(request, "example/login.html")
-    if request.method == "POST":
-        username = request.POST.get("username", "")
-        password = request.POST.get("password", "")
+# def mapping(request):
+#     if request.method == "GET":
+#         return render(request, "example/login.html")
+#     if request.method == "POST":
+#         username = request.POST.get("username", "")
+#         password = request.POST.get("password", "")
+#
+#         if username == password == "admin":
+#             return redirect("main")
 
-        if username == password == "admin":
-            return redirect("main")
 
-
+@login_required
 def main(request):
     if request.method == "GET":
-        referer_page = request.META.get("HTTP_REFERER", "")
-        referrer_path = urlparse(referer_page).path
-
-        if referrer_path != "/example/login" and referrer_path != "/example/main":
-            return HttpResponseForbidden("Access Forbidden")
+        # referer_page = request.META.get("HTTP_REFERER", "")
+        # referrer_path = urlparse(referer_page).path
+        #
+        # if referrer_path != "/example/login" and referrer_path != "/example/main":
+        #     return HttpResponseForbidden("Access Forbidden")
 
         pegawai_list = Pegawai.objects.all()
         subvar_list = SubVariabel.objects.all()
@@ -45,49 +48,149 @@ def main(request):
         return redirect("main")
 
 
+@login_required
 def skor(request):
     if request.method == "GET":
         skors = []
         data = defaultdict(lambda: defaultdict(dict))
         pegawais = Pegawai.objects.all()
-
         variabel = Variabel.objects.all()
+        subvars = SubVariabel.objects.all()
 
         nama = None
+        lanjut = True
         for pegawai in pegawais:
-            skors.append(Skor.objects.filter(pegawai=pegawai).select_related('sub_variabel'))
-        # print(skors)
-        for x in range(len(pegawais)):
-            # print(pegawais[x].nama)
-            for angka in skors[x]:
-                selisih = angka.skor - angka.sub_variabel.standar
-                normal = 6 - abs(selisih) * 0.5
-                if selisih < 0:
-                    normal -= 0.5
-                try:
-                    data[f'{angka.pegawai.nama}'][f'{angka.sub_variabel.variabel.nama}'][
-                        f'{angka.sub_variabel.faktor}'].append(normal)
-                except KeyError:
-                    data[f'{angka.pegawai.nama}'][f'{angka.sub_variabel.variabel.nama}'][
-                        f'{angka.sub_variabel.faktor}'] = [normal]
+            skor = Skor.objects.filter(pegawai=pegawai).select_related('sub_variabel')
+            if skor.count() != (subvars.count() * pegawais.count()):
+                lanjut = False
+                break
+            else:
+                skors.append(Skor.objects.filter(pegawai=pegawai).select_related('sub_variabel'))
+        if not lanjut:
+            length = 0
+            return render(request, 'example/skor.html', {'length': length, 'data': []})
+        else:
+            # print(skors)
+            for x in range(len(pegawais)):
+                # print(pegawais[x].nama)
+                for angka in skors[x]:
+                    selisih = angka.skor - angka.sub_variabel.standar
+                    normal = 6 - abs(selisih) * 0.5
+                    if selisih < 0:
+                        normal -= 0.5
+                    try:
+                        data[f'{angka.pegawai.nama}'][f'{angka.sub_variabel.variabel.nama}'][
+                            f'{angka.sub_variabel.faktor}'].append(normal)
+                    except KeyError:
+                        data[f'{angka.pegawai.nama}'][f'{angka.sub_variabel.variabel.nama}'][
+                            f'{angka.sub_variabel.faktor}'] = [normal]
 
-        portion = [var.persentase for var in variabel]
+            portion = [var.persentase for var in variabel]
 
-        res = []
-        for key, value in data.items():
-            res.append(get_skor(value, portion))
+            res = []
+            for key, value in data.items():
+                res.append(get_skor(value, portion))
 
-        data = dict()
-        for i in range(len(pegawais)):
-            data[pegawais[i].nama] = res[i]
+            data = dict()
+            for i in range(len(pegawais)):
+                data[pegawais[i].nama] = res[i]
 
-        sorted_data = dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
-        print(sorted_data)
+            sorted_data = dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
 
-        return render(request, "example/skor.html", {'data': sorted_data})
+            return render(request, "example/skor.html", {'length': len(sorted_data), 'data': sorted_data})
 
 
+@login_required
 def variabel(request):
     if request.method == 'GET':
         variabel_list = Variabel.objects.all()
         return render(request, 'example/variabel.html', {'data': variabel_list})
+
+
+@login_required
+def subvariabel(request):
+    if request.method == 'GET':
+        subvariabel_list = SubVariabel.objects.all()
+        return render(request, 'example/subvariabel.html', {'data': subvariabel_list})
+
+
+@login_required
+def employee(request):
+    if request.method == 'GET':
+        employee_list = Pegawai.objects.all()
+        return render(request, 'example/employee.html', {'data': employee_list})
+
+
+@login_required
+def create_variabel(request):
+    if request.method == 'GET':
+        return render(request, 'example/add_variabel.html')
+    if request.method == 'POST':
+        nama = request.POST.get('nama')
+        faktor = request.POST.get('faktor')
+        persentase = request.POST.get('persentase')
+
+        Variabel.objects.update_or_create(nama=nama, faktor=faktor, persentase=persentase)
+
+        return redirect('variabel')
+
+
+@login_required
+def create_subvariabel(request):
+    if request.method == 'GET':
+        variabels = Variabel.objects.all()
+        return render(request, 'example/add_subvariabel.html', {'data': variabels})
+    if request.method == 'POST':
+        kode = request.POST.get('kode')
+        variabel = request.POST.get('variabel')
+        nama = request.POST.get('nama')
+        faktor = request.POST.get('faktor')
+        standar = request.POST.get('standar')
+
+        variabel_sub = Variabel.objects.filter(nama=variabel)
+        variabel_sub = variabel_sub[0]
+
+        SubVariabel.objects.update_or_create(kode=kode, variabel=variabel_sub, nama=nama, faktor=faktor,
+                                             standar=standar)
+
+        return redirect('subvariabel')
+
+
+@login_required
+def create_employee(request):
+    if request.method == 'GET':
+        return render(request, 'example/add_employee.html')
+    if request.method == 'POST':
+        id_pegawai = request.POST.get('id_pegawai')
+        nama = request.POST.get('nama')
+
+        Pegawai.objects.update_or_create(id_pegawai=id_pegawai, nama=nama)
+
+        return redirect('employee')
+
+
+@login_required
+def delete_variabel(request, var_name):
+    if request.method == 'GET':
+        record = get_object_or_404(Variabel, nama=var_name)
+        record.delete()
+
+        return redirect('variabel')
+
+
+@login_required
+def delete_subvariabel(request, kode):
+    if request.method == 'GET':
+        record = get_object_or_404(SubVariabel, kode=kode)
+        record.delete()
+
+        return redirect('subvariabel')
+
+
+@login_required
+def delete_employee(request, id_pegawai):
+    if request.method == 'GET':
+        record = get_object_or_404(Pegawai, id_pegawai=id_pegawai)
+        record.delete()
+
+        return redirect('employee')
